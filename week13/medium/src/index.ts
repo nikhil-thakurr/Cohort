819b26c735917a248c49ae4f73520f4fb81add38
Nikhil Thakur <nt34542@gmail.com>
2024-11-03 12:39:09 +0530

    Backend Setup of medium project

--- week13/medium/src/index.ts
+++ week13/medium/src/index.ts
@@ -2,13 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.post('/',async (c) => {
-  const body = await c.req.json();
-  console.log(body);
+app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
 export default app
-
-
- 
